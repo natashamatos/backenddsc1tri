b1020ae8849e0ab4c4c771d75f4eaa42895b6f70
natashamatos <natashaquero0508@gmail.com>
2026-03-09 09:39:27 -0300

    Add files via upload

--- ativlista.js
+++ ativlista.js
@@ -1,13 +1,17 @@
 // Listas
 // índice: sempre começa em 0
-//                    0           1           2            3
-const maquiagens = ["Corretivo", "Blush", "Iluminador", "Agua micelar",   "Base",   "Gloss",   "Lip Tint",    ""]
-const valores = [    24     ,     26   ,      12    ,     31    ]
-let contador = 0 // variável de início
-while(contador < maquiagens.length){ // condição de parada
-    console.log(maquiagens[contador] + " - R$ " + valores[contador])
-    contador = contador + 1 // controle do laço
+//                    0           1           2            3                 4         5            6           7               8
+const maquiagens = ["Corretivo", "Blush", "Iluminador", "Agua micelar",   "Base",   "Gloss",   "Lip Tint",    "Rimel"  ,   "Lipliner" ]
+const valores = [    30     ,     26   ,      20    ,     40          ,     100   ,     70  ,      50    ,       20    ,        15    ]
+
+function MostrarEstoque(){
+    let contador = 0 // variável de início
+    while(contador < maquiagens.length){ // condição de parada
+        console.log(maquiagens[contador] + " - R$ " + valores[contador])
+        contador = contador + 1 // controle do laço
+    }
 }
+MostrarEstoque()
 // Adicionar novos elementos na lista
 maquiagens.push("MakeByNat")
 valores.push(25)
@@ -22,4 +26,4 @@ console.log(maquiagens)
 maquiagens.splice(2, 2) // remove a partir do 2, 2 elementos
 console.log(maquiagens)
 maquiagens.splice(0, 1, "MakeByNat") // remove o indice 0 e adiciona um novo
-console.log(maquiagens)
+MostrarEstoque()
